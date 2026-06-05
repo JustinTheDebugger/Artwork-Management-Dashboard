c913c9a8598917e4dd0e21f795420d0ba1186da9
Justin <justin.tys@hotmail.com>
2026-06-05 22:34:19 +1200

    Initial version

---
+++ db.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import psycopg
+
+def get_connection():
+    return psycopg.connect(
+        host="ep-late-rain-a7epxzsy-pooler.ap-southeast-2.aws.neon.tech",
+        dbname="neondb",
+        user="neondb_owner",
+        password="npg_VsvbSpul5Ch0",
+        sslmode="require",
+        options="endpoint=ep-late-rain-a7epxzsy"
+    )
+
+# def get_products():
+
+# def get_artwork_by_product(product_code):
+
+# def get_latest_artwork():
+
+def get_all_artworks():
+
+    with get_connection() as conn:
+
+        return pd.read_sql(
+            """
+            SELECT *
+            FROM artwork_files
+            ORDER BY release_date DESC
+            """,
+            conn
+        )
