d341be2f7e32e3f0d551df6ff66d57de533475df
Justin <justin.tys@hotmail.com>
2026-06-11 20:27:14 +1200

    Added pages using naviagtion API

--- app.py
+++ app.py
@@ -1,42 +1,8 @@
 import streamlit as st
-from db import get_connection
-from db import get_all_artworks
 
-st.set_page_config(
-    page_title="Artwork Tracker",
-    layout="wide"
-)
+pg = st.navigation([
+    st.Page("pages/dashboard.py", title="Dashboard"),
+    st.Page("pages/artwork_tracker.py", title="Artwork Tracker"),
+])
 
-st.title("Artwork Management Dashboard")
-
-conn = get_connection()
-
-df = get_all_artworks()
-
-summary_df = (
-    df.groupby(
-        ["product_code", "product_name"]
-    )
-    .size()
-    .reset_index(name="artwork_count")
-)
-
-st.subheader("Products")
-
-st.dataframe(
-    summary_df,
-    use_container_width=True
-)
-
-# Filters
-
-product_name = st.text_input("Product Name")
-
-if product_name:
-    df = df[
-        df["product_name"]
-        .astype(str)
-        .str.contains(product_name, case=False)
-    ]
-
-st.dataframe(df, use_container_width=True)
+pg.run()
